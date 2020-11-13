e92217334df06c38409509702dcb5f7a96e2d0ea
AyuLearn <73938708+AyuLearn@users.noreply.github.com>
2020-11-13 09:10:18 +0530

    Update Settings
    Update Settings

--- CoderBlog/settings.py
+++ CoderBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kf%)mx7ss%&o+_*u!no$1f6ucj--c)75_q8@eqzk$_jqc7-8=4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coderbloger.herokuapp.com']
 
 
 # Application definition
