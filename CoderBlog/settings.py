b01693d43ab31d65d47d6caed99359ce412b7455
Ayush Tripathi <ayushlearn592004@gmail.com>
2020-11-13 08:34:02 +0530

    Second Commit

--- CoderBlog/settings.py
+++ CoderBlog/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/media/'
 
+STATIC_ROOT = 'staticfiles'
+
 MEDIA_ROOT = BASE_DIR / "media"
+
 STATICFILES_DIRS = [BASE_DIR / "static"]
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn CoderBlog.wsgi --log-file
